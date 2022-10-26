e83969511f36797159291d321a7099afc7c8d908
Jhonnatan Brito <jhonnatanbritooficial@gmail.com>
2022-10-26 20:00:51 -0300

    Project created

---
+++ backEnd/SendMS/src/main/java/com/SendMS/SendMS/SendMsApplication.java
@@ -0,0 +1,13 @@
+package com.SendMS.SendMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMsApplication.class, args);
+	}
+
+}
---
+++ backEnd/SendMS/src/test/java/com/SendMS/SendMS/SendMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SendMS.SendMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
